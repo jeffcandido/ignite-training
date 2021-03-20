e2c34b8d63cb03189d3dbf84dcbb09cd95beeed5
Jefferson Candido <jefferson.ufu@gmail.com>
2021-03-20 02:19:11 -0300

    Adding POST course

--- track-nodejs/01-nodejs-fundamentals/src/index.js
+++ track-nodejs/01-nodejs-fundamentals/src/index.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
 const courses = [
-    "Engenharia de Telecomunicações",
-    "Ciência da Computação",
-    "História",
-    "Música",
-    "Nutrição",
-    "Filosofia"
-]
+  "Engenharia de Telecomunicações",
+  "Ciência da Computação",
+  "História",
+  "Música",
+  "Nutrição",
+  "Filosofia",
+];
 
 app.get("/courses", (_req, res) => {
-    res.json(courses);
+  res.json(courses);
+});
+
+app.post("/courses", (req, res) => {
+  const { course } = req.body;
+
+  courses.push(course);
+
+  res.json(courses);
 });
 
 app.listen(3333);
