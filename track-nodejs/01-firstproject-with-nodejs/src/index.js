000bc030e23e7725e4b509855bee8dfc15ab6fda
Jefferson Candido <jefferson.ufu@gmail.com>
2021-03-22 01:25:11 -0300

    Route to delete customer's account

--- track-nodejs/01-firstproject-with-nodejs/src/index.js
+++ track-nodejs/01-firstproject-with-nodejs/src/index.js
@@ -131,6 +131,14 @@ app.get("/accounts", verifyExistsAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/accounts", verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
 app.get("/balances", verifyExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
