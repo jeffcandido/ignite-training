bdf3fbb23b8aba3dd910de7262b7b1aaea3e2f8c
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-14 23:53:21 -0300

    Listing Categories

--- track-nodejs/03-continuing-api/rentx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ track-nodejs/03-continuing-api/rentx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,10 +1,16 @@
-import { Category } from '../../model/Category';
-import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+import { inject, injectable } from 'tsyringe';
 
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+
+@injectable()
 class ListCategoriesUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository,
+  ) {}
 
-  execute(): Category[] {
+  async execute(): Promise<Category[]> {
     const categories = this.categoriesRepository.list();
 
     return categories;
