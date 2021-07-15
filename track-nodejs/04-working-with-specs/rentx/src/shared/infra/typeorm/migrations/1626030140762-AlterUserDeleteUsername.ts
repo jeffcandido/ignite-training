d3a6be38cac6081a9cb58f6550e6b54b1176ac28
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-15 00:41:24 -0300

    Started Chapter IV

---
+++ track-nodejs/04-working-with-specs/rentx/src/shared/infra/typeorm/migrations/1626030140762-AlterUserDeleteUsername.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterUserDeleteUsername1626030140762 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'username');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('user', new TableColumn({ name: 'username', type: 'string', isUnique: true }));
+  }
+}
