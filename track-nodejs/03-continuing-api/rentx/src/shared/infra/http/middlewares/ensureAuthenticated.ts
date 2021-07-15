c25ef15d65ff940c725747979d80f1255d6281ea
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-14 23:54:12 -0300

    Ensuring user authentication middleware

---
+++ track-nodejs/03-continuing-api/rentx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAutheticated(request: Request, response: Response, next: NextFunction): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(token, String(process.env.JWT_SECRET_KEY)) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
+
+    request.user = {
+      id: user.id,
+    };
+
+    return next();
+  } catch (error) {
+    throw new AppError('Invalid token!', 401);
+  }
+}
