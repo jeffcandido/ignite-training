102e28594cf002195aa91ccd21eacb998aa52476
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-14 22:22:07 -0300

    Adjusting entrypoint file for new architecture.

---
+++ track-nodejs/03-continuing-api/rentx/src/shared/infra/http/server.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+
+import '../typeorm';
+import '@shared/container';
+
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+
+import { AppError } from '@shared/errors/AppError';
+
+import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal Server Error - ${err.message}`,
+  });
+});
+
+app.listen(Number(process.env.RENTX_API_PORT) || 3333, () => console.log(`Server started on port ${process.env.RENTX_API_PORT || 3333}`));
