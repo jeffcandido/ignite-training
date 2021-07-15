66c14834cf836fe58b33d4e3f2c2c43b0e93b7cc
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-14 22:23:12 -0300

    Adjusting routes files for new architecture.

---
+++ track-nodejs/03-continuing-api/rentx/src/shared/infra/http/routes/specifications.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+
+import { ensureAutheticated } from '../middlewares/ensureAuthenticated';
+
+const specificationsRoutes = Router();
+
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post('/', ensureAutheticated, createSpecificationController.handle);
+
+export { specificationsRoutes };
